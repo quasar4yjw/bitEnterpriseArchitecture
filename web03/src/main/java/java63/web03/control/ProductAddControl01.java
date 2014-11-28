39799c401d8899240c9e6051a8bb6fac81aebc9f
Jungwoo YEOM <quasar4yjw@gmail.com>
2014-11-28 18:34:43 +0900

    auth filter active on web.xml

--- web03/src/main/java/java63/web03/control/ProductAddControl01.java
+++ web03/src/main/java/java63/web03/control/ProductAddControl01.java
@@ -4,12 +4,10 @@ import java.util.Map;
 import java63.web03.dao.MakerDao;
 import java63.web03.dao.ProductDao;
 import java63.web03.domain.Product;
-import java63.web03.util.FileUploadHelper;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
